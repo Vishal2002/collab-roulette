bf5b462b6878610d68cbb0723a33df139e664dbb
Vishal <vishalsharma05052002@gmail.com>
2024-10-16 15:44:52 +0530

    trying to fix bug in build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
-//@ts-nocheck
-import NextAuth, { AuthOptions } from "next-auth"
-import TwitchProvider from "next-auth/providers/twitch"
-import { SupabaseAdapter } from "@auth/supabase-adapter"
-
-export const authOptions:AuthOptions = {
-  providers: [
-    TwitchProvider({
-      clientId: process.env.TWITCH_CLIENT_ID,
-      clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    }),
-  ],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    secret: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }),
-}
+import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
 
 const handler = NextAuth(authOptions)
 
---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+//@ts-nocheck
+import { AuthOptions } from "next-auth"
+import TwitchProvider from "next-auth/providers/twitch"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+
+export const authOptions:AuthOptions = {
+  providers: [
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    secret: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }),
+}
