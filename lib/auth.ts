754027b93436dd7b80be09afe03b602b7aa87b95
Vishal <vishalsharma05052002@gmail.com>
2024-10-17 11:41:57 +0530

    added secret in authOptions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,27 @@
-//@ts-nocheck
-import { AuthOptions } from "next-auth"
-import TwitchProvider from "next-auth/providers/twitch"
-import { SupabaseAdapter } from "@auth/supabase-adapter"
+import { AuthOptions } from "next-auth";
+import TwitchProvider from "next-auth/providers/twitch";
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 
-export const authOptions:AuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     TwitchProvider({
-      clientId: process.env.TWITCH_CLIENT_ID,
-      clientSecret: process.env.TWITCH_CLIENT_SECRET,
+      clientId: process.env.TWITCH_CLIENT_ID!,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    secret: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }),
-}
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }) as any, // Type assertion needed due to compatibility issues
+  callbacks: {
+    async session({ session, user }) {
+      if (session?.user) {
+        //@ts-ignore
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+
+};
